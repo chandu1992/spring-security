398629f3116ccbb7af20d96f64fbdb3b6d0d7fa0
chandrashekharbhaupande <chpande55@gmail.com>
2025-06-30 22:04:43 +0530

    security enable

--- src/main/java/com/jarvis/security/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/jarvis/security/SecurityConfig/SecurityConfig.java
@@ -1,18 +1,23 @@
 package com.jarvis.security.SecurityConfig;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/student/**", "/api/department/**")
+                        .requestMatchers(
+                                "/api/student/**"
+//                                "/api/department/**"
+                        )
                         .permitAll()
                         //.authenticated()
                         .anyRequest().denyAll()   // 403 Forbidden for every other path
