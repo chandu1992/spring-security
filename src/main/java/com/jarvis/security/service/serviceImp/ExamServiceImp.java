b4e030856950d2b9f0101303a006a8c62221f0c3
chandrashekharbhaupande <chpande55@gmail.com>
2026-08-08 19:04:53 +0530

    chnage in exam service impl

--- src/main/java/com/jarvis/security/service/serviceImp/ExamServiceImp.java
+++ src/main/java/com/jarvis/security/service/serviceImp/ExamServiceImp.java
@@ -15,7 +15,7 @@ public class ExamServiceImp implements ExamService {
     ExamRepo examRepo;
     @Override
     public ExamInfo schedulExam(ExamInfo exam) {
-        System.out.println("chandra");
+        System.out.println("chandrs");
         return examRepo.save(exam);
     }
 
